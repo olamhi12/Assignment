1e0ba9b180421fb27ea7db591bc09b94748ba340
adamsolaide76@gmail.com <adamsolaide76@gmail.com>
2025-07-12 18:22:27 +0100

    commit

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ Adams.addEventListener("click", ()=>{
     const checkbtn = document.createElement("button")
     checkbtn.innerHTML= '<i class="fa-solid fa-circle-check"></i>'
     div.appendChild(checkbtn)
+    checkbtn.classList.add("CheckTask")
   
 
     const deletebtn = document.createElement("button")
     deletebtn.innerHTML= '<i class="fa-solid fa-trash"></i>'
     div.appendChild(deletebtn)
+    deletebtn.classList.add("deleteTask")
     
     it.innerText = `${Course.value}`
     
@@ -35,6 +37,12 @@ Adams.addEventListener("click", ()=>{
     date.value=""
 
     console.log(it);
-    
+    checkbtn.addEventListener("click", ()=>{
+        checkbtn.parentElement.style.textDecoration = "line-through red"
+    })
+    deletebtn.addEventListener("click", (e)=>{
+        let target = e.target;
+        target.parentElement.parentElement.remove();
+    })
 })
  
